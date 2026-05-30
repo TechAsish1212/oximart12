efe2ad92e1fb94797f4a0bdc876e5105f24a79fe
TechAsish1212 <gitasish1212@gmail.com>
2026-05-30 13:26:22 +0530

    feat: start building card functionallity

--- frontend/src/customer/pages/Product/ProductsCard.tsx
+++ frontend/src/customer/pages/Product/ProductsCard.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './ProductCard.css'
 
-const ProductsCard = () => {
+const ProductsCard = ({ item }: any) => {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [currentImage, setCurrentImage] = useState(1)
+
   return (
-    <div>ProductsCard</div>
+    <div className='group px-4 relative '>
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className='relative w-[250px] sm:w-full h-[350px] overflow-hidden  '>
+        {item.images.map((image: string, index: number) => {
+          return (
+            <img 
+            src={image} 
+            alt="" 
+            className='card-media object-top' 
+            key={index} 
+            style={{transform:`translateX(${(index-currentImage)*100}%)`}}
+            />
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
